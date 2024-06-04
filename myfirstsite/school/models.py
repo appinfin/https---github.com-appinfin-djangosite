451035e52c32d70e70522cd8bd4465f982e7f88d
appinfin <desktop.developer@yandex.ru>
2024-06-04 23:05:09 +0500

    HP add product

--- myfirstsite/school/models.py
+++ myfirstsite/school/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # # Create your models here.
 class Product(models.Model):
     prod_name = models.CharField(max_length=64)
-
     price = models.DecimalField(max_digits=7, decimal_places=2, max_length=9, null=True, blank=True)
     in_stock = models.BooleanField(default=False)
     width = models.PositiveSmallIntegerField()
