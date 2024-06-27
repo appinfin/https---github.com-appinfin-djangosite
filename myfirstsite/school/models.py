ea98e177adf62adcb70b0b1e92cce7d0eab6d623
appinfin <desktop.developer@yandex.ru>
2024-06-27 05:41:23 +0500

    HP edit admin ru-RU

--- myfirstsite/school/models.py
+++ myfirstsite/school/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # # Create your models here.
 class Product(models.Model):
-    prod_name = models.CharField(max_length=64)
-    price = models.DecimalField(max_digits=7, decimal_places=2, max_length=9, null=True, blank=True)
-    in_stock = models.BooleanField(default=False)
+    prod_name = models.CharField(max_length=64, verbose_name='Наименование товара')
+    price = models.DecimalField(max_digits=7,
+                                decimal_places=2,
+                                max_length=9,
+                                null=True, blank=True,
+                                verbose_name='Цена')
+    in_stock = models.BooleanField(default=False, verbose_name='Наличие')
     width = models.PositiveSmallIntegerField()
     heigth = models.PositiveSmallIntegerField()
     foto = models.ImageField(upload_to='photos/%Y/%m/%d/',
@@ -13,5 +17,13 @@ class Product(models.Model):
                              width_field='width',
                              height_field='heigth')
     
+    def __unicode__(self):
+        return self.prod_name
+    
     def __str__(self) -> str:
         return super().__str__()
+    
+    class Meta:
+        verbose_name='Товар'
+        verbose_name_plural='Товары'
+        ordering=['price']
